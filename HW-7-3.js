24b5428d1087b2bf6c467ec07e70ae41ab218260
Dmirr <dmitriyrodionow@gmail.com>
2022-11-06 14:54:46 +0300

    tried to solve

--- HW-7-3.js
+++ HW-7-3.js
@@ -1,7 +1,22 @@
 class Button {
-  constructor(width, height, color) {
+  constructor(width, height, type, color) {
+    this.width = width;
+    this.height = height;
+    this.type = type;
     this.color = color;
-    this.brand = brand;
-    this.model = model;
+    this.props = [width, height, type, color];
+  }
+  onClick() {
+    console.log(this.props);
+  }
+  testButton() {
+    console.log(
+      `Я не понимаю , где взять информацию для выполнения этого задания`
+    );
   }
 }
+let newButton = new Button(38, 38, "button", "green");
+
+console.log(newButton);
+console.log(newButton.onClick());
+console.log(newButton.testButton());
