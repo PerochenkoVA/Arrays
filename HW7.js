59f76d67b35a981f37672fb66b75d6ffcd7e01e3
Dmirr <dmitriyrodionow@gmail.com>
2022-11-06 13:32:25 +0300

    added switch

--- HW7.js
+++ HW7.js
@@ -1,15 +1,21 @@
-let postomat = ["null", "null", "null", "4-your package", "null"];
+let postomat = ["null", "null", "null", "A001DFX0", "null"];
 let clientAccount = 100;
 console.log(postomat[3]);
 
-let confirmationSMS = ("your parcel is in the cell at number 4", "A001DFX0");
+let codeSMS = "A001DFX0";
 
-if ((confirmationSMS = "A001DFX0")) {
-  console.log(
-    postomat[3],
-    "You received a parcel from box 4 and your bill is 0"
-  );
-} else {
-  confirmationSMS = "another text";
-  console.log("nothing");
+switch (codeSMS) {
+  case "A001DFX0":
+    console.log("You received a parcel from box 4 and your bill is 0()");
+    break;
 }
+
+// if ((confirmationSMS = "A001DFX0")) {
+//   console.log(
+//     postomat[3],
+//     "You received a parcel from box 4 and your bill is 0"
+//   );
+// } else {
+//   confirmationSMS = "another text";
+//   console.log("nothing");
+// }
