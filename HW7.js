ddf4b760a2367a85b5383b63bfe957b8b0c24853
Dmirr <dmitriyrodionow@gmail.com>
2022-11-07 13:25:41 +0300

    fixed

--- HW7.js
+++ HW7.js
@@ -1,13 +1,16 @@
 let postomat = ["null", "null", "null", "A001DFX0", "null"];
 let clientAccount = 100;
 console.log(postomat[3]);
-
 let codeSMS = "A001DFX0";
-
-switch (codeSMS) {
-  case "A001DFX0":
-    console.log("You received a parcel from box 4 and your bill is 0()");
-    break;
+for (codeSMS of postomat) {
+  switch (codeSMS) {
+    case "A001DFX0":
+      console.log("You received a parcel from box 4 and your bill is 0");
+      break;
+    case "":
+      console.log("Please try again");
+      break;
+  }
 }
 
 // if ((confirmationSMS = "A001DFX0")) {
